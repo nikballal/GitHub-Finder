89af893e46c79dadb188b71dc394f803c06d8651
nikballal <nikhil.ballal1994@gmail.com>
2022-05-04 15:17:50 +1000

    display users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Spinner from "../layouts/Spinner";
+import UserItem from "./UserItem";
 
 //empty array on line 8, since no dependencies, just want the component to load
 
@@ -28,8 +29,8 @@ function UserResults() {
     // if loading is 'false', return the data
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user, index) => (
-          <h3 key={index}>{user.login}</h3>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
