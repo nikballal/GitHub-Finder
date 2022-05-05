1e9822411e224d9e68f5c720d083917ff764acbc
nikballal <nikhil.ballal1994@gmail.com>
2022-05-05 16:45:51 +1000

    search users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,20 +13,24 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //get initial users (testing purposes)
-  const fetchUsers = async () => {
+  //Get seach results
+  const searchUsers = async (text) => {
     setLoading(); //from line 34
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `${GITHUB_TOKEN}`,
       },
     });
 
-    const data = await response.json();
+    const { items } = await response.json();
     dispatch({
       type: "GET_USERS",
-      payload: data,
+      payload: items,
     });
   };
 
@@ -34,7 +38,7 @@ export const GithubProvider = ({ children }) => {
   const setLoading = () => dispatch({ type: "SET_LOADING" });
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{ users: state.users, loading: state.loading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
