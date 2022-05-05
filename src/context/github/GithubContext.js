0ecf85e13dbd7175897eebb62c1e6822453d0f03
nikballal <nikhil.ballal1994@gmail.com>
2022-05-05 16:28:27 +1000

    user search component added

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,14 +8,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   //get initial users (testing purposes)
   const fetchUsers = async () => {
-    setLoading();
+    setLoading(); //from line 34
 
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
